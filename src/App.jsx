9369dc21af3d1fcf1eeca7681194d68e8e48f6e7
MI AKASH <mszharul1990gamil.com>
2024-04-23 22:01:29 +0600

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import "./App.css"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { FiSettings } from "react-icons/fi"
+import { TooltipComponent } from "@syncfusion/ej2-react-popups"
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./Components"
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Bar,Line, Area, Pie, Kanban, Editor
+, ColorPicker, ColorMapping, Customers, Financial } from "./Pages"
 
 const App = () => {
+  const activeMenu = true;
+
   return (
-    <div  className="text-8xl bg-red-500">App</div>
+    <div className="text-black ">
+      <BrowserRouter>
+        <div className='flex relative dark:bg-main-dark-bg'>
+          <div className='fixed right-4 bottom-4' style={{zIndex: '1000'}}>
+            <TooltipComponent content="Setting" position="Top">
+              <button type='button' className='rounded-lg text-3xl hover:drop-shadow-xl hover:bg-light-gray p-3 text-white' style={{background: "blue", borderRadius: "50%"}}>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+
+          {activeMenu ? (
+            <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+              <Sidebar />
+            </div>
+          ) : (
+            <div className='dark:bg-secondary-dark-bg bg-white w-0'>
+              <Sidebar />
+            </div>
+          )}
+          <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
+            <div className='w-full fixed md:static bg-main-bg dark:bg-main-dark-bg navbar'>
+              <Navbar />
+            </div>
+          </div>
+
+          <div>
+            <Routes>
+              {/* Dashboard */}
+              <Route path="/" element={<Ecommerce />} />
+              <Route path="/ecommerce" element={<Ecommerce />} />
+              {/* Pages */}
+              <Route path="/order" element={<Orders />} />
+              <Route path="/employees" element={<Employees />} />
+              <Route path="/customers" element={<Customers />} />
+              {/* Apps */}
+              <Route path="/kanban" element={<Kanban />} />
+              <Route path="/editor" element={<Editor />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/color-picker" element={<ColorPicker />} />
+              {/* Charts */}
+              <Route path="/line" element={<Line />} />
+              <Route path="/area" element={<Area />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/financial" element={<Financial />} />
+              <Route path="/color-mapping" element={< ColorMapping />}/>
+              <Route path="/pyramid" element={<Pyramid />} />
+              <Route path="/stacked" element={<Stacked />} />
+
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { SiShopware } from "react-icons/si";
+import { MdOutlineCancel } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { links } from "../data/dummy";
+
+const Sidebar = () => {
+  const activeMenu = true;
+  const activeLink = "flex items-center gap-4 pl-4 pt-3 pb-2.5 rounded-lg text-md m-2"
+  const normalLink = "flex items-center gap-4 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-800 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2"
+
+  return (
+    <div className="ml-5 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+      {activeMenu && (
+        <>
+          <div className="flex justify-between items-center">
+            <Link to="/" onClick={() => {}} className="items-center gap-3 flex text-xl mt-1 ml-3 dark:text-white text-slate-900 font-bold tracking-tighter uppercase">
+              <SiShopware className="text-3xl"/>
+              <span>Shoppy</span>
+            </Link>
+            <TooltipComponent content="Menu" position="BottomCenter">
+              <button type="button" className="text-xl rounded-full p-3 hover:bg-light-gray mt-1 md:hidde block ">
+                <MdOutlineCancel />
+              </button>
+            </TooltipComponent>
+          </div>
+          <div className="mt-5">
+            {links.map((item)=>(
+              <div>
+                <p className="text-gray-400 dark:text-gray-400 mt-4 m-3 uppercase">{item.title}</p>
+                {item.links.map((link)=>(
+                  <NavLink
+                  to={`${link.name}`} 
+                  className={({isActive})=>(isActive ? activeLink : normalLink)}
+                  >
+                    {link.icon}
+                    <span className="capitalize">{link.name}</span>
+                  </NavLink>
+                ))}
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Sidebar;
