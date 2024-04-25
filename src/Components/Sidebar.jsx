4005f579d83ede8d7a44d5afb6038c0da9809048
MI AKASH <121384260+MIAKASH19@users.noreply.github.com>
2024-04-25 23:24:07 +0600

    new commit

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -7,35 +7,49 @@ import { links } from "../data/dummy";
 
 const Sidebar = () => {
   const activeMenu = true;
-  const activeLink = "flex items-center gap-4 pl-4 pt-3 pb-2.5 rounded-lg text-md m-2"
-  const normalLink = "flex items-center gap-4 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-800 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2"
+  const activeLink =
+    "flex items-center gap-4 pl-4 pt-3 pb-2.5 rounded-lg text-md m-1";
+  const normalLink =
+    "flex items-center gap-4 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-800 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-1";
 
   return (
     <div className="ml-5 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
         <>
           <div className="flex justify-between items-center">
-            <Link to="/" onClick={() => {}} className="items-center gap-3 flex text-xl mt-1 ml-3 dark:text-white text-slate-900 font-bold tracking-tighter uppercase">
-              <SiShopware className="text-3xl"/>
+            <Link
+              to="/"
+              onClick={() => {}}
+              className="items-center gap-3 flex text-xl mt-1 ml-3 dark:text-white text-slate-900 font-bold tracking-tighter uppercase"
+            >
+              <SiShopware className="text-3xl" />
               <span>Shoppy</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
-              <button type="button" className="text-xl rounded-full p-3 hover:bg-light-gray mt-1 md:hidde block ">
+              <button
+                type="button"
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-1 md:hidde block "
+              >
                 <MdOutlineCancel />
               </button>
             </TooltipComponent>
           </div>
           <div className="mt-5">
-            {links.map((item)=>(
+            {links.map((item) => (
               <div>
-                <p className="text-gray-400 dark:text-gray-400 mt-4 m-3 uppercase">{item.title}</p>
-                {item.links.map((link)=>(
+                <p className="text-gray-400 dark:text-gray-400 mt-4 m-3 uppercase">
+                  {item.title}
+                </p>
+                {item.links.map((link) => (
                   <NavLink
-                  to={`${link.name}`} 
-                  className={({isActive})=>(isActive ? activeLink : normalLink)}
+                    to={`${link.name}`}
+                    className={({ isActive }) =>
+                      isActive ? activeLink : normalLink
+                    }
+                    onClick={()=>{}}
                   >
                     {link.icon}
-                    <span className="capitalize">{link.name}super Nova</span>
+                    <span className="capitalize">{link.name}</span>
                   </NavLink>
                 ))}
               </div>
